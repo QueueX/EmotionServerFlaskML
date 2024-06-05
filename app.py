9d08b8d035f894b450cde87036de9fa77812b87a
Kinok0 <truthpruthbg@gmail.com>
2024-06-06 01:12:11 +0300

    Russian model added

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ import pickle
 from flask import Flask, request, jsonify
 
 with open("model.pkl", 'rb') as file1:
-    model = pickle.load(file1)
+    en_model = pickle.load(file1)
 with open("count.pkl", 'rb') as file2:
-    count = pickle.load(file2)
+    en_count = pickle.load(file2)
+with open("ru_model.pkl", 'rb') as file3:
+    ru_model = pickle.load(file3)
+with open("ru_count.pkl", 'rb') as file4:
+    ru_count = pickle.load(file4)
 
 app = Flask(__name__)
 
@@ -12,16 +16,29 @@ app = Flask(__name__)
 @app.route('/getLabel', methods=['GET'])
 def process_text_route():
     text = request.args.get('text')
-    if not text:
-        return jsonify({'error': 'Missing text parameter'}), 400
+    flag = request.args.get('flag')
+    if not text and not flag:
+        return jsonify({'error': 'Missing text or flag parameter'}), 400
 
-    result = get_label(text)
+    if flag == '0':
+        result = get_label_en(text)
+    elif flag == '1':
+        result = get_label_ru(text)
+    else:
+        return jsonify({'error': "I don't know that flag"}), 400
+
     return jsonify({'label': result})
 
 
-def get_label(text):
-    predict_matrix = count.transform([text]).toarray()
-    result = model.predict(predict_matrix)
+def get_label_en(text):
+    predict_matrix = en_count.transform([text]).toarray()
+    result = en_model.predict(predict_matrix)
+    return int(result[0])
+
+
+def get_label_ru(text):
+    predict_matrix = ru_count.transform([text])
+    result = ru_model.predict(predict_matrix)
     return int(result[0])
 
 
